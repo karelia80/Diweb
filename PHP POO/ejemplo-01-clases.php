6078c88994adcd57fd3bf7b0d52b10470647e62f
Blanca <soyunpaketito@gmail.com>
2024-03-10 10:23:52 +0100

    JSejercicios hechos

---
+++ PHP POO/ejemplo-01-clases.php
@@ -0,0 +1,124 @@
+<?php
+//Ejemplo 01 Clases
+
+//las clases tienen que ir antes del ISSET
+
+//Manual php pag 311 manual
+class Camion
+{
+    //atributos
+    public $modelo = "Volvo FH electric";
+    public $precio = 500000;
+    public $electrico = "true";
+    //Constructor
+    public function __construct($modelo, $precio, $electrico)
+    {
+        $this->modelo = $modelo;
+        $this->precio = $precio;
+        $this->electrico = $electrico;
+    }
+    public function __toString() //para imprimir
+    {
+        $valorElectrico = "No";
+        if($this->electrico){
+            $valorElectrico="Si";
+        }
+        return "CAMION: Modelo $this->modelo <br>
+                        Precio: $this->precio <br>
+                        Electrico: $valorElectrico ";
+
+      
+    }
+}
+if (isset($_REQUEST['enviar'])) {
+    $texto = $_REQUEST['texto'];  //texto
+    $num = $_REQUEST['num'];        //number
+    $opcion = $_REQUEST['opcion']; //boolean
+
+      //crear un OBJETO para imprimir el resultado
+      $camion = new Camion($texto, $num, $opcion);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 01 PHP POO Clases</title>
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap.bundle.min.js"></script>
+    <style>
+        h2 {
+            color: #1121A7;
+            text-decoration: underline;
+        }
+
+        .form-label {
+            color: #1121A7;
+        }
+
+        label[for="opcion1"],
+        label[for="opcion2"] {
+            color: #1121A7;
+        }
+
+        p {
+            color: #1121A7;
+        }
+    </style>
+
+</head>
+
+<body class="w-70 p-3 m-3">
+
+    <main class="bg-primary text-white py-4 w-100 text-center fixed-top">
+        <h1>Clases</h1>
+    </main><br><br><br><br>
+
+    <section class="container pt-3 m-4">
+        <h2>PHP con POO: Clases</h2><br>
+    </section>
+
+
+    <section>
+
+        <p class="alert alert-info">
+            <?php
+            if (isset($_REQUEST['enviar'])) {
+                // echo  $texto . "<br>";
+                // echo  $num . "<br>";
+                // echo  $opcion . "<br>";
+
+                echo $camion;
+            }
+
+            ?>
+        </p>
+
+    </section>
+    <section class="w-50 pt-3 m-4">
+        <form action="#" method="post" class="form">
+            <label for="texto" class="form-label">Modelo</label>
+            <input type="text" name="texto" id="texto" class="form-control"><br>
+
+            <label for="num" class="form-label">Precio</label>
+            <input type="number" name="num" id="num" class="form-control"><br>
+
+            <p>Electrico</p>
+            <input type="radio" name="opcion" id="opcion1" class="form-check-input" checked value="1">
+            <label for="opcion1" class="form-check-label">Si</label><br>
+            <input type="radio" name="opcion" id="opcion2" class="form-check-input" value="0">
+            <label for="opcion2" class="form-check-label">No</label><br><br>
+
+            <input type="submit" value="Enviar" name="enviar" class="btn btn-outline-info">
+
+        </form>
+    </section>
+
+
+
+</body>
+
+</html>
